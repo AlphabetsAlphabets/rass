aa1e4096e34d22697a2bf9bd1906193e7a66edf8
Yap Jia Hong <yapjiahong2003@gmail.com>
2023-04-11 07:33:54 +0800

    Error handling done with `?` instead of `unwrap`
    - It is necessary for library crates to use `?` as opposed to `unwrap`
    
    Need to use `?`: https://rust-lang.github.io/api-guidelines/documentation.html#c-question-mark

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
-use std::fs::File;
-use std::io::{Read, Write};
-use std::path::Path;
-
-use pkcs8::{DecodePrivateKey, DecodePublicKey, EncodePrivateKey, EncodePublicKey, LineEnding};
-use rsa::{Pkcs1v15Encrypt, PublicKey, RsaPrivateKey, RsaPublicKey};
+use std::fs;
+use std::io;
 
 mod tests;
 
+use pkcs8::{DecodePrivateKey, DecodePublicKey, EncodePrivateKey, EncodePublicKey, LineEnding};
+use rsa::{errors::Result as RsaResult, Pkcs1v15Encrypt, PublicKey, RsaPrivateKey, RsaPublicKey};
+
+
+/// Contains the key pairs and their names. This struct is to use the key pairs and to retrieve
+/// them. It can also be used to create new key pairs.
 pub struct Keys<'names> {
     priv_key: RsaPrivateKey,
     priv_key_name: &'names str,
@@ -18,7 +20,7 @@ impl<'names> Keys<'names> {
     /// Creates key pairs.
     /// # Parameters
     /// - `priv_key_name` & `pub_key_name`: The name of the keys. If the keys are saved to disk
-    /// with `self.write_to_disk` the values for these variables will be the file name. 
+    /// with `self.write_to_disk` the values for these variables will be the file name.
     pub fn new(priv_key_name: &'names str, pub_key_name: &'names str) -> Self {
         let mut rng = rand::thread_rng();
 
@@ -39,30 +41,30 @@ impl<'names> Keys<'names> {
     /// - `priv_key`: Path to private key file.
     /// - `password`: The password used to encrypt the private key.
     /// - `pub_key`: Path to public key file.
-    pub fn retreive_keys(priv_key_path: &'names str, password: &str, pub_key_path: &'names str) -> Self {
-        let enc_priv_key = Path::new(priv_key_path);
-
-        // Get *encrypted* private key
-        let mut file = File::open(enc_priv_key).expect("Unable to find private key.");
-        let mut content = String::new();
-
-        file.read_to_string(&mut content).unwrap();
+    pub fn retreive_keys(
+        priv_key_path: &'names str,
+        password: &str,
+        pub_key_path: &'names str,
+    ) -> Result<Self, io::Error> {
+        // Get *encrypted* private key first
+        let mut pem = fs::read_to_string(priv_key_path)?;
 
         // Decrypt encrypted private key
-        let priv_key = RsaPrivateKey::from_pkcs8_encrypted_pem(&content, password).unwrap();
-        content.clear();
+        let priv_key = RsaPrivateKey::from_pkcs8_encrypted_pem(&pem, password)
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))?;
+        pem.clear();
 
-        // Get public key
-        file = File::open(pub_key_path).expect("Unable to find public key.");
-        file.read_to_string(&mut content).unwrap();
-        let pub_key = RsaPublicKey::from_public_key_pem(&content).unwrap();
+        // Then get public key
+        pem = fs::read_to_string(pub_key_path)?;
+        let pub_key = RsaPublicKey::from_public_key_pem(&pem)
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))?;
 
-        Self {
+        Ok(Self {
             priv_key,
             priv_key_name: priv_key_path,
             pub_key,
             pub_key_name: pub_key_path,
-        }
+        })
     }
 }
 
@@ -72,34 +74,34 @@ impl Keys<'_> {
     /// # Parameters
     /// - `priv_key_pass`: The password used to encrypt the private key.
     /// - `folder`: The folder to write the keys to. If left empty will default to cwd.
-    pub fn write_to_disk(&self, priv_key_pass: &str, folder: &str) {
-        let folder = if folder.is_empty() {
-            "."
-        } else {
-            folder
-        };
+    pub fn write_to_disk(&self, priv_key_pass: &str, folder: &str) -> Result<(), io::Error> {
+        let folder = if folder.is_empty() { "." } else { folder };
 
         let priv_key_pem = self
             .priv_key
             .to_pkcs8_encrypted_pem(&mut rand::thread_rng(), priv_key_pass, LineEnding::LF)
-            .unwrap();
-        let pub_key_pem = self.pub_key.to_public_key_pem(LineEnding::LF).unwrap();
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))?;
+
+        let pub_key_pem = self
+            .pub_key
+            .to_public_key_pem(LineEnding::LF)
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))?;
 
         let priv_key_path = format!("{}/{}", folder, self.priv_key_name);
-        let mut file = File::create(priv_key_path).expect("Unable to create private key pem.");
-        file.write_all(priv_key_pem.as_bytes()).unwrap();
+        fs::write(priv_key_path, priv_key_pem.as_bytes())?;
 
         let pub_key_path = format!("{}/{}", folder, self.pub_key_name);
-        file = File::create(pub_key_path).expect("Unable to create pub key pem.");
-        file.write_all(pub_key_pem.as_bytes()).unwrap();
+        fs::write(pub_key_path, pub_key_pem.as_bytes())?;
+
+        Ok(())
     }
 
-    pub fn seal(&self, plaintext: &[u8]) -> rsa::errors::Result<Vec<u8>> {
+    pub fn seal(&self, plaintext: &[u8]) -> RsaResult<Vec<u8>> {
         let mut rng = rand::thread_rng();
         self.pub_key.encrypt(&mut rng, Pkcs1v15Encrypt, plaintext)
     }
 
-    pub fn unseal(&self, ciphertext: &[u8]) -> rsa::errors::Result<Vec<u8>> {
+    pub fn unseal(&self, ciphertext: &[u8]) -> RsaResult<Vec<u8>> {
         self.priv_key.decrypt(Pkcs1v15Encrypt, ciphertext)
     }
 }
